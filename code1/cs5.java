0b6cb9e4259c21bca46b43a31200145b454a8611
lljh <lljh1992@naver.com>
2023-05-26 14:23:51 +0900

    .

---
+++ code1/cs5.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class cs5 {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+    float A = sc.nextInt();
+    float B = sc.nextInt();
+
+    System.out.println(A / B);
+  }
+}
