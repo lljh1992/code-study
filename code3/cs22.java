ef738c924d3a4271c8a25c6abdd95cdd08da9892
lljh <lljh1992@naver.com>
2023-06-01 17:43:34 +0900

    .

---
+++ code3/cs22.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class cs22 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int c = sc.nextInt();
+    int d;
+
+    for (d = 0; d < c; d++){
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      System.out.println(a + b);
+    }
+
+  }
+}
